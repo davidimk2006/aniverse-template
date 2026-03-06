cb1085edf46625a1ccf4a15264ea13f38238ef19
davidimk2006 <71768272+davidimk2006@users.noreply.github.com>
2026-03-07 06:19:57 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,4 @@
-const SUPABASE_URL = "https://awmczzjgnzdntlhobqkp.supabase.co"
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
+const supabaseUrl = "https://awmczzjgnzdntlhobqkp.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
 
-window.db = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-)
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey)
