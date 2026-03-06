694a7301ca487260f9ab9bd3ad953921a377e79a
davidimk2006 <71768272+davidimk2006@users.noreply.github.com>
2026-03-07 06:40:59 +0700

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,11 +2,25 @@
 import { supabaseClient } from "./supabase.js"
 
 async function loadAnime() {
+
   const { data, error } = await supabaseClient
     .from("anime")
     .select("*")
+    .order("created_at", { ascending: false })
 
-  console.log(data)
+  if (error) {
+    console.error(error)
+    return
+  }
+
+  const container = document.getElementById("anime-list")
+
+  data.forEach(anime => {
+    const div = document.createElement("div")
+    div.innerHTML = `<h3>${anime.title}</h3>`
+    container.appendChild(div)
+  })
+
 }
 
 loadAnime()
