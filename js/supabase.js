4c326ba12d450e9ec868ed65e74eb3ae1a5c9c65
davidimk2006 <71768272+davidimk2006@users.noreply.github.com>
2026-03-07 06:57:40 +0700

    Update and rename supabase.js to js/supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+const SUPABASE_URL = "https://awmczzjgnzdntlhobqkp.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+export { supabaseClient }
--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-const supabaseUrl = "https://awmczzjgnzdntlhobqkp.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
-
-export const supabaseClient = createClient(supabaseUrl, supabaseKey)
