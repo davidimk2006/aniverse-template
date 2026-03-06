53ac8af90a2b436190c0d7f64ff29df129527024
davidimk2006 <71768272+davidimk2006@users.noreply.github.com>
2026-03-07 06:45:53 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -3,4 +3,4 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = "https://awmczzjgnzdntlhobqkp.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabaseClient = createClient(supabaseUrl, supabaseKey)
