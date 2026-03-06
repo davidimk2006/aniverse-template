1e12f29d589ef91160bf48260b3d4a3f9d2d9339
davidimk2006 <71768272+davidimk2006@users.noreply.github.com>
2026-03-07 05:18:43 +0700

    Update supabase.js

--- api/verse v4/js/supabase.js
+++ api/verse v4/js/supabase.js
@@ -1,6 +1,6 @@
 
 const SUPABASE_URL = "PASTE_SUPABASE_URL"
-const SUPABASE_KEY = "PASTE_SUPABASE_ANON_KEY"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF3bWN6empnbnpkbnRsaG9icWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MDgwMjMsImV4cCI6MjA4NzI4NDAyM30.r7NSaDIe12ycfA5hzsXL0T0wTNB-N0aHlVr-noeaPNI"
 
 const supabase = window.supabase.createClient(
 SUPABASE_URL,
